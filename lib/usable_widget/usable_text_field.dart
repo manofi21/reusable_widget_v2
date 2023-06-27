e3e047e8d23de0e4b6b9e9bb5b7cb94d1b035b6a
unknown <jeremimanogi@gmail.com>
2023-06-27 14:55:43 +0700

    create usable field example

---
+++ lib/usable_widget/usable_text_field.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../master_widget/master_text_field.dart';
+
+class UsableTextField extends StatelessWidget {
+  const UsableTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width - 20,
+      child: MasterTextField(
+        labelText: '',
+        controller: TextEditingController(),
+        value: 'Text Filled',
+        hint: 'Hint',
+        usePrefix: true,
+        prefixWidget: const Icon(Icons.access_alarm),
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(width: 1),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/usable_widget/usable_text_field_with_row.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../master_widget/master_text_field.dart';
+
+class UsableTextFieldWithRow extends StatelessWidget {
+  const UsableTextFieldWithRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(width: 1, style: BorderStyle.solid),
+          borderRadius: BorderRadius.circular(10)),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      width: MediaQuery.of(context).size.width - 20,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Icon(Icons.access_alarm),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: MasterTextField(
+                labelText: '',
+                controller: TextEditingController(),
+                hint: 'Hint',
+                border: InputBorder.none,
+                textAlignVertical: -.7,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
